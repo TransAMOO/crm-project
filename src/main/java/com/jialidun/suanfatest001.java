5e83398cc6677694720cefd31305cf8d22243fc6
name <1813187257@qq.com>
2022-08-12 22:38:38 +0800

    first commit

---
+++ src/main/java/com/jialidun/suanfatest001.java
@@ -0,0 +1,11 @@
+package com.jialidun;
+
+public class suanfatest001 {
+    public static void main(String[] args) {
+        fun1();
+    }
+
+    private static void fun1() {
+        System.out.println("Hello World !");
+    }
+}
