9691da33967b9ca67faf5450b569337df4d7310f
name <1813187257@qq.com>
2022-08-13 22:49:25 +0800

    first commit on newbranch001

--- src/main/java/com/jialidun/suanfatest001.java
+++ src/main/java/com/jialidun/suanfatest001.java
@@ -9,6 +9,7 @@ public class suanfatest001 {
         System.out.println("Hello World !");
         System.out.println("第二次修改后");
         System.out.println("第三次修改后");
+        System.out.println("在第三次修改后基础上向分支newbranch pull");
 
     }
 }
